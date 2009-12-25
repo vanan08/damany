92458f9550a9f4b4fc96edacd35e28814c3c6a69
shenbinsc <shenbinsc@gmail.com>
2009-12-25 17:37:26 +0800

    ??RemoteImging.Test??

---
+++ RemoteImaging/RemoteImaging.Test/SVMWrapperTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gallio.Framework;
+using MbUnit.Framework;
+using MbUnit.Framework.ContractVerifiers;
+using OpenCvSharp;
+
+namespace RemoteImaging.Test
+{
+    [TestFixture]
+    public class SVMWrapperTest
+    {
+        [SetUp]
+        public void SVMInitialize()
+        {
+            RemoteImaging.SVMWrapper.InitSvmData(100*100, 40);
+        }
+
+
+        [Test]
+        [Row("d:\abc.jpg")]
+        public void SVMPredict(string fileFullPath)
+        {
+            IplImage ipl = IplImage.FromFile(fileFullPath);
+
+            System.Drawing.Bitmap bmp = ipl.ToBitmap();
+
+            float[] imgData = ImageProcess.NativeIconExtractor.ResizeIplTo(ipl, 100, 100, BitDepth.U8, 1);
+
+            double result =  RemoteImaging.SVMWrapper.SvmPredict(imgData);
+
+            System.Diagnostics.Trace.WriteLine(result);
+        }
+    }
+}
