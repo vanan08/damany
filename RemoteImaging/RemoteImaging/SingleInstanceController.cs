2ab9245c3dd89fe48ec278bb62bb366000fb547e
shenbinsc <shenbinsc@gmail.com>
2010-02-01 14:45:02 +0800

    ???????????????

---
+++ RemoteImaging/RemoteImaging/SingleInstanceController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RemoteImaging
+{
+    class SingleInstanceController : Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase
+    {
+        public SingleInstanceController()
+        {
+            IsSingleInstance = true;
+
+            this.Startup += new Microsoft.VisualBasic.ApplicationServices.StartupEventHandler(SingleInstanceController_Startup);
+        }
+
+        void SingleInstanceController_Startup(object sender, Microsoft.VisualBasic.ApplicationServices.StartupEventArgs e)
+        {
+
+#if !DEBUG
+
+            if (!Util.VerifyKey())
+            {
+                RegisterForm form = new RegisterForm();
+                DialogResult res = form.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    Application.Restart();
+                }
+                
+                e.Cancel = true;
+                return;
+            }
+#endif
+
+            if (e.CommandLine.Count  > 0)
+            {
+                Program.directory = e.CommandLine[0];
+            }
+
+
+            Program.faceSearch = new FaceSearchWrapper.FaceSearch();
+            Program.motionDetector = new MotionDetectWrapper.MotionDetector();
+
+        }
+
+
+        protected override void OnCreateMainForm()
+        {
+            this.MainForm = new RealtimeDisplay.MainForm();
+        }
+    }
+}
