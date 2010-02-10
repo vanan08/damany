a488edf6c83445e2b8adf922902fa476a6caf06c
shenbinsc <shenbinsc@gmail.com>
2010-02-10 14:32:47 +0800

    ?????????

---
+++ E-Police/E-Police.BusinessLogic/LicensePlateNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace E_Police.DTO
+{
+    public class LicensePlate
+    {
+        public string AreaCode
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public string LicenseNumber
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public LicensePlateCategory Category
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ E-Police/E-Police.BusinessLogic/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace E_Police.DTO
+{
+    public class Vehicle
+    {
+        public VehicleCategory Category
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public VehicleOwner Owner
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public LicensePlate LicensePlateNumber
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
