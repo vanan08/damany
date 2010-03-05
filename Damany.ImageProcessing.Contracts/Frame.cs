4dafaf3f00896011e5e02d2a1916f639e6fb9dd7
benny <shenbinsc@gmail.com>
2010-03-05 14:37:53 +0800

    Temporary check in

---
+++ Damany.ImageProcessing.Contracts/Frame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Damany.ImageProcessing.Contracts
+{
+    public class Frame
+    {
+        public byte[] BitmapData { get; set; }
+        public DateTime CapturedAt { get; set; }
+        public IFrameStream CapturedFrom { get; set; }
+    }
+}
---
+++ Damany.ImageProcessing.Contracts/MotionFrame.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenCvSharp;
+
+namespace Damany.ImageProcessing.Contracts
+{
+    public class MotionFrame : Frame
+    {
+        public IList<CvRect> MotionRectangles { get; set; }
+    }
+}
