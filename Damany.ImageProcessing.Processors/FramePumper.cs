02e9aea793aa14968cee7025744b87ca8e52881e
benny <shenbinsc@gmail.com>
2010-03-10 22:48:44 +0800

    temp checkin

---
+++ Damany.ImageProcessing.Processors/FramePumper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Damany.Imaging.Processors
+{
+    using Damany.Imaging.Contracts;
+
+    public class FramePumper
+    {
+        public FramePumper(IFrameStream source)
+        {
+            if (source == null)
+                throw new ArgumentNullException("source", "source is null.");
+
+            this.frameSource = source;
+        }
+
+        public void Pump()
+        {
+            if (this.MotionDetector == null)
+            {
+                throw new InvalidOperationException("MotionDetector is null");
+            }
+
+            var frame = this.frameSource.RetrieveFrame();
+            this.MotionDetector.DetectMotion(frame);
+        }
+
+        public MotionDetector MotionDetector { get; set; }
+
+        IFrameStream frameSource;
+    }
+}
