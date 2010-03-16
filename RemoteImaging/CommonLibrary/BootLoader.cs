fc0f0ecbcb7105641e3d3560c4adde2a9bbacd5a
benny <shenbinsc@gmail.com>
2010-03-16 16:32:12 +0800

    ???????????????

--- RemoteImaging/CommonLibrary/BootLoader.cs
+++ RemoteImaging/CommonLibrary/BootLoader.cs
@@ -12,20 +12,22 @@ namespace Damany.RemoteImaging.Common
             = new List<Damany.Imaging.Processors.FaceSearchController>();
         public Damany.PortraitCapturer.Repository.PersistenceService repository;
 
-        public void Load()
+        public void Load(string databaseRoot)
         {
             ConfigurationManager config = ConfigurationManager.GetDefault();
 
             ReportProgress(0, "初始化数据库");
-            SearchLineBuilder.GetDefaultPersistenceService();
-            this.repository = SearchLineBuilder.GetDefaultPersistenceService();
+            this.repository = Damany.PortraitCapturer.Repository.PersistenceService.CreateDefault( databaseRoot );
             ReportProgress(50, "数据库初始化成功");
+
+            var portraitWriter = new Damany.Imaging.Handlers.PersistenceWriter(this.repository);
 
             foreach (var cam in config.GetCameras())
             {
                 string url = @"D:\20090505";
                 ReportProgress(50, "初始化摄像头: " + cam.Location.ToString());
                 var controller = SearchLineBuilder.BuildNewSearchLine(cam);
+                controller.RegisterPortraitHandler(portraitWriter);
                 this.controllers.Add(controller);
                 ReportProgress(90, "初始化摄像头: " + cam.Location.ToString() + "成功");
 
