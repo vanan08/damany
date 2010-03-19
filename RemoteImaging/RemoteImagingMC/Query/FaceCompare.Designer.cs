53fe356ad2c0924c63ac054056bdb36277477126
benny <shenbinsc@gmail.com>
2010-03-20 00:07:09 +0800

    face compare

--- RemoteImaging/RemoteImagingMC/Query/FaceCompare.Designer.cs
+++ RemoteImaging/RemoteImagingMC/Query/FaceCompare.Designer.cs
@@ -28,34 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.currentPic = new System.Windows.Forms.PictureBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchTo = new DevExpress.XtraEditors.TimeEdit();
+            this.searchFrom = new DevExpress.XtraEditors.TimeEdit();
             this.choosePic = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.targetPic = new System.Windows.Forms.PictureBox();
-            this.timeEdit1 = new DevExpress.XtraEditors.TimeEdit();
-            this.timeEdit2 = new DevExpress.XtraEditors.TimeEdit();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.currentPic = new System.Windows.Forms.PictureBox();
             this.splitter1 = new System.Windows.Forms.Splitter();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.label3 = new System.Windows.Forms.Label();
+            this.faceList = new System.Windows.Forms.ListView();
             this.label4 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.compareButton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.panel1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.targetPic)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.timeEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.timeEdit2.Properties)).BeginInit();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.currentPic)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.searchTo.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchFrom.Properties)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.targetPic)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.compareButton);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.groupBox3);
             this.panel1.Controls.Add(this.groupBox2);
@@ -64,8 +68,40 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 459);
+            this.panel1.Size = new System.Drawing.Size(200, 782);
             this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(6, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 63);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "选定一张照片后,从数据库中比较与该照片相似的图片";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.currentPic);
+            this.groupBox3.Location = new System.Drawing.Point(3, 505);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(194, 274);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "待比较图片";
+            // 
+            // currentPic
+            // 
+            this.currentPic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.currentPic.Location = new System.Drawing.Point(3, 16);
+            this.currentPic.Name = "currentPic";
+            this.currentPic.Size = new System.Drawing.Size(188, 255);
+            this.currentPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.currentPic.TabIndex = 0;
+            this.currentPic.TabStop = false;
             // 
             // groupBox2
             // 
@@ -73,14 +109,58 @@
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.timeEdit2);
-            this.groupBox2.Controls.Add(this.timeEdit1);
+            this.groupBox2.Controls.Add(this.searchTo);
+            this.groupBox2.Controls.Add(this.searchFrom);
             this.groupBox2.Location = new System.Drawing.Point(3, 315);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(194, 137);
             this.groupBox2.TabIndex = 2;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "时间范围";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "结束时间:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "起始时间:";
+            // 
+            // searchTo
+            // 
+            this.searchTo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchTo.EditValue = new System.DateTime(2010, 3, 19, 0, 0, 0, 0);
+            this.searchTo.Location = new System.Drawing.Point(9, 91);
+            this.searchTo.Name = "searchTo";
+            this.searchTo.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.searchTo.Properties.Mask.EditMask = "f";
+            this.searchTo.Size = new System.Drawing.Size(179, 22);
+            this.searchTo.TabIndex = 1;
+            // 
+            // searchFrom
+            // 
+            this.searchFrom.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchFrom.EditValue = new System.DateTime(2010, 3, 19, 0, 0, 0, 0);
+            this.searchFrom.Location = new System.Drawing.Point(9, 40);
+            this.searchFrom.Name = "searchFrom";
+            this.searchFrom.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.searchFrom.Properties.Mask.EditMask = "f";
+            this.searchFrom.Size = new System.Drawing.Size(179, 22);
+            this.searchFrom.TabIndex = 0;
             // 
             // choosePic
             // 
@@ -116,98 +196,23 @@
             this.targetPic.TabIndex = 0;
             this.targetPic.TabStop = false;
             // 
-            // timeEdit1
-            // 
-            this.timeEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.timeEdit1.EditValue = new System.DateTime(2010, 3, 19, 0, 0, 0, 0);
-            this.timeEdit1.Location = new System.Drawing.Point(9, 40);
-            this.timeEdit1.Name = "timeEdit1";
-            this.timeEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton()});
-            this.timeEdit1.Properties.Mask.EditMask = "f";
-            this.timeEdit1.Size = new System.Drawing.Size(179, 22);
-            this.timeEdit1.TabIndex = 0;
-            // 
-            // timeEdit2
-            // 
-            this.timeEdit2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.timeEdit2.EditValue = new System.DateTime(2010, 3, 19, 0, 0, 0, 0);
-            this.timeEdit2.Location = new System.Drawing.Point(9, 91);
-            this.timeEdit2.Name = "timeEdit2";
-            this.timeEdit2.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
-            new DevExpress.XtraEditors.Controls.EditorButton()});
-            this.timeEdit2.Properties.Mask.EditMask = "f";
-            this.timeEdit2.Size = new System.Drawing.Size(179, 22);
-            this.timeEdit2.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "起始时间:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(58, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "结束时间:";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox3.Controls.Add(this.currentPic);
-            this.groupBox3.Location = new System.Drawing.Point(3, 471);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(194, 190);
-            this.groupBox3.TabIndex = 3;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "待比较图片";
-            // 
-            // currentPic
-            // 
-            this.currentPic.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.currentPic.Location = new System.Drawing.Point(3, 16);
-            this.currentPic.Name = "currentPic";
-            this.currentPic.Size = new System.Drawing.Size(188, 171);
-            this.currentPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.currentPic.TabIndex = 0;
-            this.currentPic.TabStop = false;
-            // 
             // splitter1
             // 
             this.splitter1.Location = new System.Drawing.Point(200, 0);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 459);
+            this.splitter1.Size = new System.Drawing.Size(3, 782);
             this.splitter1.TabIndex = 1;
             this.splitter1.TabStop = false;
             // 
-            // listView1
+            // faceList
             // 
-            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listView1.Location = new System.Drawing.Point(203, 24);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(519, 435);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            // 
-            // label3
-            // 
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label3.Location = new System.Drawing.Point(6, 9);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(182, 63);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "选定一张照片后,从数据库中比较与该照片相似的图片";
+            this.faceList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.faceList.LargeImageList = this.imageList1;
+            this.faceList.Location = new System.Drawing.Point(203, 24);
+            this.faceList.Name = "faceList";
+            this.faceList.Size = new System.Drawing.Size(519, 758);
+            this.faceList.TabIndex = 2;
+            this.faceList.UseCompatibleStateImageBehavior = false;
             // 
             // label4
             // 
@@ -227,26 +232,42 @@
             this.openFileDialog1.Filter = "Jpeg 文件|*.jpg";
             this.openFileDialog1.RestoreDirectory = true;
             // 
+            // compareButton
+            // 
+            this.compareButton.Location = new System.Drawing.Point(63, 476);
+            this.compareButton.Name = "compareButton";
+            this.compareButton.Size = new System.Drawing.Size(75, 23);
+            this.compareButton.TabIndex = 5;
+            this.compareButton.Text = "比对";
+            this.compareButton.UseVisualStyleBackColor = true;
+            this.compareButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(128, 128);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
             // FaceCompare
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(722, 459);
-            this.Controls.Add(this.listView1);
+            this.ClientSize = new System.Drawing.Size(722, 782);
+            this.Controls.Add(this.faceList);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.panel1);
             this.Name = "FaceCompare";
             this.Text = "人脸比对查询";
             this.panel1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.targetPic)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.timeEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.timeEdit2.Properties)).EndInit();
             this.groupBox3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.currentPic)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.searchTo.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchFrom.Properties)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.targetPic)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -262,12 +283,14 @@
         private System.Windows.Forms.PictureBox currentPic;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private DevExpress.XtraEditors.TimeEdit timeEdit2;
-        private DevExpress.XtraEditors.TimeEdit timeEdit1;
+        private DevExpress.XtraEditors.TimeEdit searchTo;
+        private DevExpress.XtraEditors.TimeEdit searchFrom;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Splitter splitter1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView faceList;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button compareButton;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
--- RemoteImaging/RemoteImagingMC/Query/FaceCompare.cs
+++ RemoteImaging/RemoteImagingMC/Query/FaceCompare.cs
@@ -14,6 +14,26 @@ namespace RemoteImaging.Query
         public FaceCompare()
         {
             InitializeComponent();
+
+            this.searchFrom.EditValue = DateTime.Now.AddDays(-1);
+            this.searchTo.EditValue = DateTime.Now;
+
+            this.targetPic.Paint += new PaintEventHandler(targetPic_Paint);
+            this.compareButton.Click += new EventHandler(compareButton_Click);
+        }
+
+        void compareButton_Click(object sender, EventArgs e)
+        {
+            this.presenter.CompareClicked();
+        }
+
+        public void AttachPresenter(FaceComparePresenter presenter)
+        {
+            this.presenter = presenter;
+        }
+
+        void targetPic_Paint(object sender, PaintEventArgs e)
+        {
         }
 
         private void choosePic_Click(object sender, EventArgs e)
@@ -26,23 +46,118 @@ namespace RemoteImaging.Query
             var img = Damany.Util.Extensions.MiscHelper.FromFileBuffered(this.openFileDialog1.FileName);
             this.targetPic.Image = img;
 
-            var ipl = OpenCvSharp.IplImage.FromBitmap((Bitmap) img);
+            this.ipl = OpenCvSharp.IplImage.FromBitmap((Bitmap)img);
 
             var searcher = new FaceSearchWrapper.FaceSearch();
 
             var frame = new Damany.Imaging.Common.Frame(ipl);
+            frame.MotionRectangles.Add(new OpenCvSharp.CvRect(0, 0, ipl.Width, ipl.Height));
             searcher.AddInFrame(frame);
             var portraits = searcher.SearchFaces();
             var face = portraits.FirstOrDefault();
 
             if (face == null)
             {
-                MessageBox.Show("未定位到人脸");
+                MessageBox.Show("未定位到人脸", this.Text,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
+            this.faceRect = face.Portraits[0].FacesRectForCompare;
 
-
+            this.targetPic.Invalidate();
         }
+
+        public DateTime SearchFrom
+        {
+            get
+            {
+                return (DateTime)this.searchFrom.EditValue;
+            }
+        }
+
+        public DateTime SearchTo
+        {
+            get
+            {
+                return (DateTime)this.searchTo.EditValue;
+            }
+        }
+
+        public Image CurrentImage
+        {
+            set
+            {
+                if (InvokeRequired)
+                {
+                    Action<Image> action = img => this.CurrentImage = img;
+
+                    this.BeginInvoke(action, value);
+                    return;
+                }
+
+
+                if (this.currentPic.Image != null)
+                {
+                    this.currentPic.Image.Dispose();
+                }
+
+                this.currentPic.Image = value;
+            }
+        }
+
+        public OpenCvSharp.IplImage Image
+        {
+            get
+            {
+                return this.ipl;
+            }
+        }
+
+        public Rectangle FaceRect
+        {
+            get
+            {
+                return this.faceRect;
+            }
+        }
+
+        public void ClearFaceList()
+        {
+            this.imageList1.Images.Clear();
+        }
+
+        public void AddPortrait(Damany.Imaging.Common.Portrait p)
+        {
+            if (this.InvokeRequired)
+            {
+                Action<Damany.Imaging.Common.Portrait> action
+                    = this.AddPortrait;
+
+                this.BeginInvoke(action, p);
+                return;
+            }
+
+            this.imageList1.Images.Add(p.GetImage().ToBitmap());
+
+            var item = new ListViewItem();
+            item.Text = p.CapturedAt.ToString();
+            item.ImageIndex = this.imageList1.Images.Count - 1;
+
+            this.faceList.Items.Add(item);
+
+            p.Dispose();
+        }
+
+        private Rectangle faceRect;
+        private FaceComparePresenter presenter;
+        private OpenCvSharp.IplImage ipl;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.presenter.CompareClicked();
+        }
+
+
     }
 }
