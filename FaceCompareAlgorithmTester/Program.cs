65298215ca75a5aeb84d621eb0f1b676a3224066
Administrator <Administrator@Bin-X200>
2010-04-04 23:56:22 +0800

    temp check in, Algorithm Test UI is done

--- FaceCompareAlgorithmTester/Program.cs
+++ FaceCompareAlgorithmTester/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition.Primitives;
 using System.Linq;
 using System.Windows.Forms;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
 
 namespace FaceCompareAlgorithmTester
 {
@@ -13,9 +16,22 @@ namespace FaceCompareAlgorithmTester
         [STAThread]
         static void Main()
         {
+            var controller = LoadPlugins();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static Controller LoadPlugins()
+        {
+            var catelog = new DirectoryCatalog(@".\");
+            var container = new CompositionContainer(catelog);
+
+            var controller = new Controller();
+            container.ComposeParts(controller);
+
+            return controller;
         }
     }
 }
