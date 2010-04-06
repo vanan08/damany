ea70620b81814fad8c7471abd5b540c48d59cdd5
shenbinsc <shenbinsc@gmail.com>
2010-04-07 00:56:58 +0800

    ?????????????????

--- RemoteImaging/Damany.Util/Extensions/MiscHelper.cs
+++ RemoteImaging/Damany.Util/Extensions/MiscHelper.cs
@@ -39,6 +39,13 @@ namespace Damany.Util.Extensions
             }
         }
 
+        public static DateTime RoundToMinute(this DateTime time)
+        {
+            return new DateTime(time.Year, time.Month, time.Day,
+                                time.Hour, time.Minute, 0, time.Kind);
+        }
+
+
 
     }
 }
