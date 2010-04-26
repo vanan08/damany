682fdf969314a9dc0ab59226bc9fab49f1177276
shenbinsc <shenbinsc@gmail.com>
2010-04-26 15:43:39 +0800

    ???????

---
+++ RemoteImaging/RemoteImaging/LicensePlate/ILicensePlateRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteImaging.LicensePlate
+{
+    public interface ILicensePlateRepository
+    {
+        void Save(LicensePlateInfo licensePlateInfo);
+
+        IEnumerable<LicensePlateInfo> GetLicensePlates(Damany.Util.DateTimeRange dateTimeRange);
+        IEnumerable<LicensePlateInfo> GetLicensePlates(string licensePlateNumber);
+    }
+}
---
+++ RemoteImaging/RemoteImaging/LicensePlate/LicensePlateInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteImaging.LicensePlate
+{
+    public class LicensePlateInfo
+    {
+        public DateTime CaptureTime { get; set; }
+        public string LicensePlateNumber { get; set; }
+        public int CapturedFrom { get; set; }
+        public System.Drawing.Image LicensePlateImage { get; set; }
+    }
+}
