719a16dac0e8178edc7d2a1f39618c9a1184a9c1
shenbinsc <shenbinsc@localhost>
2010-05-04 10:02:24 +0800

    add missing file

---
+++ RemoteImaging/RemoteImaging/LicensePlate/LicensePlateRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoteImaging.LicensePlate
+{
+    public class LicensePlateRepository
+    {
+        private readonly ILicensePlateDataProvider _dataProvider;
+
+        public LicensePlateRepository(ILicensePlateDataProvider dataProvider)
+        {
+            if (dataProvider == null) throw new ArgumentNullException("dataProvider");
+            _dataProvider = dataProvider;
+        }
+
+
+        public void Save(LicensePlateInfo licensePlateInfo)
+        {
+            
+        }
+    }
+}
