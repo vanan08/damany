24479f0610b1dd3dae56482ab05a40f00ca60cbc
shenbinsc <shenbinsc@gmail.com>
2010-05-18 19:20:09 +0800

    ??????????

---
+++ RemoteImaging/FaceLibraryManager/ViewModel/SuspectsCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using Damany.Imaging.Common;
+
+namespace FaceLibraryManager.ViewModel
+{
+    public class SuspectsCollection : ObservableCollection<PersonOfInterest>
+    {
+    }
+}
