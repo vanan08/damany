88afd90164b6c1742945a71e817795292d7302b8
shenbinsc <shenbinsc@gmail.com>
2010-05-19 20:55:53 +0800

    ????????

---
+++ RemoteImaging/FaceLibraryManager/View/AddNewFace.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FaceLibraryManager.View
+{
+    /// <summary>
+    /// Interaction logic for AddNewFace.xaml
+    /// </summary>
+    public partial class AddNewFace : Window
+    {
+        public AddNewFace()
+        {
+            InitializeComponent();
+        }
+    }
+}
