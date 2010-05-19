31a06aa5ca8407e4323b6103efd3d436901794ee
shenbinsc <shenbinsc@gmail.com>
2010-05-20 00:14:31 +0800

    ????????????

--- RemoteImaging/FaceLibraryManager/View/AddNewFace.xaml.cs
+++ RemoteImaging/FaceLibraryManager/View/AddNewFace.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -10,6 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Damany.Util;
+using FaceSearchWrapper;
+using OpenCvSharp;
+using Damany.Imaging.Extensions;
 
 namespace FaceLibraryManager.View
 {
@@ -18,9 +24,63 @@ namespace FaceLibraryManager.View
     /// </summary>
     public partial class AddNewFace : Window
     {
+        private static FaceSearchWrapper.FaceSearch searcher 
+            = new FaceSearch();
+
+
+
         public AddNewFace()
         {
             InitializeComponent();
         }
+
+        private void Window_DragOver(object sender, DragEventArgs e)
+        {
+            bool isFile = e.Data.GetDataPresent(DataFormats.FileDrop);
+            if (isFile)
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[]) e.Data.GetData(DataFormats.FileDrop);
+                string extenstion = System.IO.Path.GetExtension(files[0]);
+                if (string.Compare(extenstion, ".jpg", false) == 0)
+                {
+                    imageFile.Content = files[0];
+
+                    using (var ipl = IplImage.FromFile(files[0]))
+                    {
+                        var facesRect = ipl.LocateFaces(searcher);
+
+                        if (facesRect.Length > 0)
+                        {
+                            ipl.DrawRect(facesRect[0], CvColor.Black, 2);
+
+                            var ms = new MemoryStream();
+                            var bitmap = ipl.ToBitmap();
+
+
+                            var bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                                bitmap.GetHbitmap(),
+                                IntPtr.Zero,
+                                Int32Rect.Empty,
+                                BitmapSizeOptions.FromEmptyOptions());
+                            this.suspectInfo1.image1.Source = bitmapSource;
+                        }
+
+                    }
+
+                }
+            }
+
+
+        }
+
+    
     }
 }
