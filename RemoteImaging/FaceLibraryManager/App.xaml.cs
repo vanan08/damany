3759b39ac0793750b55f9e38cf10425fa9e791c6
shenbinsc <shenbinsc@gmail.com>
2010-05-19 17:20:37 +0800

    add FaceLibraryManager project

---
+++ RemoteImaging/FaceLibraryManager/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace FaceLibraryManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var manager = SuspectsRepository.SuspectsRepositoryManager.LoadFrom(@"d:\imglib");
+            var vm = new ViewModel.SuspectsListViewModel(manager);
+
+            var mainView = new MainView();
+            mainView.DataContext = vm;
+            mainView.Show();
+        }
+    }
+}
