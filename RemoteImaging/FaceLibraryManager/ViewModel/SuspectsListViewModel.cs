8c2b4770a4c973f1bbdc8914c26c96d646ec6ac2
shenbinsc <shenbinsc@gmail.com>
2010-05-19 16:20:26 +0800

    before use theme

--- RemoteImaging/FaceLibraryManager/ViewModel/SuspectsListViewModel.cs
+++ RemoteImaging/FaceLibraryManager/ViewModel/SuspectsListViewModel.cs
@@ -1,3 +1,5 @@
+using System.Windows;
+using System.Windows.Input;
 using SuspectsRepository;
 
 namespace FaceLibraryManager.ViewModel
@@ -5,8 +7,34 @@ namespace FaceLibraryManager.ViewModel
     public class SuspectsListViewModel
     {
         private readonly SuspectsRepositoryManager _repositoryManager;
+        private RelayCommand _deleteSuspectCommand;
 
-        public SuspectsCollection AllSuspects { get; set; }
+        public SuspectsCollection AllSuspects { get; private set; }
+
+        public Damany.Imaging.Common.PersonOfInterest SelectedSuspect { get; set; }
+
+        public double ZoomFactor { get; set; }
+
+        public ICommand AddNewSuspect { get; private set; }
+
+        public ICommand DeleteSuspect
+        {
+            get
+            {
+                if (_deleteSuspectCommand == null)
+                {
+                    _deleteSuspectCommand = new RelayCommand(
+                        param => this.Delete(),
+                        param => this.CanDelete
+                        );
+                }
+
+                return _deleteSuspectCommand;
+            }
+        }
+
+        
+
 
         public SuspectsListViewModel(SuspectsRepository.SuspectsRepositoryManager repositoryManager)
         {
@@ -16,6 +44,25 @@ namespace FaceLibraryManager.ViewModel
             foreach (var personOfInterest in repositoryManager.Peoples)
             {
                 AllSuspects.Add(personOfInterest);
+            }
+
+            ZoomFactor = 1;
+
+
+        }
+
+
+        void Delete()
+        {
+            AllSuspects.Remove(SelectedSuspect);
+            ZoomFactor *= 1.2;
+        }
+
+        bool CanDelete
+        {
+            get
+            {
+                return SelectedSuspect != null;
             }
         }
     }
