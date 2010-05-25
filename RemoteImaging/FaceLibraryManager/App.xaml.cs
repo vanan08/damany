4f3743f572da2c6ddf5f90b7aeee9a6a28f8d642
shenbinsc <shenbinsc@gmail.com>
2010-05-25 16:57:35 +0800

    ????????????

--- RemoteImaging/FaceLibraryManager/App.xaml.cs
+++ RemoteImaging/FaceLibraryManager/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace FaceLibraryManager
 {
@@ -15,32 +16,55 @@ namespace FaceLibraryManager
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+
             base.OnStartup(e);
 
-            var args = Environment.GetCommandLineArgs();
-            if (args.Length == 1)
+            try
             {
-                MessageBox.Show("请指定人脸特征库目录!", "", MessageBoxButton.OK, MessageBoxImage.Stop);
-                Application.Current.Shutdown(0);
-                return;
+                var args = Environment.GetCommandLineArgs();
+                if (args.Length == 1)
+                {
+                    MessageBox.Show("请指定人脸特征库目录!", "", MessageBoxButton.OK, MessageBoxImage.Stop);
+                    Application.Current.Shutdown(0);
+                    return;
+                }
+
+                var repositoryDirectory = args[1];
+                SuspectsRepository.SuspectsRepositoryManager repositoryManager;
+                if (System.IO.Directory.Exists(repositoryDirectory))
+                {
+                    repositoryManager = SuspectsRepository.SuspectsRepositoryManager.LoadFrom(repositoryDirectory);
+                }
+                else
+                {
+                    repositoryManager = SuspectsRepository.SuspectsRepositoryManager.CreateNewIn(repositoryDirectory);
+                }
+
+                var vm = new ViewModel.SuspectsListViewModel(repositoryManager);
+
+                var mainView = new MainView();
+                mainView.DataContext = vm;
+                mainView.Show();
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex);
             }
 
-            var repositoryDirectory = args[1];
-            SuspectsRepository.SuspectsRepositoryManager repositoryManager;
-            if (System.IO.Directory.Exists(repositoryDirectory))
-            {
-                repositoryManager = SuspectsRepository.SuspectsRepositoryManager.LoadFrom(repositoryDirectory);
-            }
-            else
-            {
-                repositoryManager = SuspectsRepository.SuspectsRepositoryManager.CreateNewIn(repositoryDirectory);
-            }
 
-            var vm = new ViewModel.SuspectsListViewModel(repositoryManager);
+        }
 
-            var mainView = new MainView();
-            mainView.DataContext = vm;
-            mainView.Show();
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            ShowError(e.Exception);
+            e.Handled = true;
+        }
+
+        private void ShowError(Exception e)
+        {
+            MessageBox.Show("更新特征库的过程中发生异常：\n\n" + e.Message, "异常", MessageBoxButton.OK, MessageBoxImage.Error);
+
         }
     }
 }
