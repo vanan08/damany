b968676f8b82f72edf61b97c5a4ed344acf901b7
shenbinsc <shenbinsc@gmail.com>
2010-06-02 01:43:14 +0800

    minor

--- CameraSearcher/CameraSearcher/App.xaml.cs
+++ CameraSearcher/CameraSearcher/App.xaml.cs
@@ -12,5 +12,14 @@ namespace CameraSearcher
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+
+            var mainForm = new MainWindow();
+            mainForm.Show();
+        }
+
     }
 }
