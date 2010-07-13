e932db17aa75b037860c5d2ab966f1594366533d
shenbinsc <shenbinsc@gmail.com>
2010-07-13 20:16:11 +0800

    ????TopShelf???service?

---
+++ RemoteImaging/WatchDogService/MockHeartBeatMonitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace WatchDog
+{
+    class MockHeartBeatMonitor : HeartBeatMonitorBase
+    {
+        protected override void Work(CancellationToken token)
+        {
+            while (true)
+            {
+                token.ThrowIfCancellationRequested();
+                Thread.Sleep(TimeToReport);
+                InvokeHeartBeatStopped(EventArgs.Empty);
+
+            }
+        }
+    }
+}
