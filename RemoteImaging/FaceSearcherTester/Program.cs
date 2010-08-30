08b3ac1dfc676b5392ef685713f88dc64b92c996
shenbinsc <shenbinsc@gmail.com>
2010-08-30 19:18:34 +0800

    ???????????

---
+++ RemoteImaging/FaceSearcherTester/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FaceSearcherTester
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DevExpress.UserSkins.BonusSkins.Register();
+            DevExpress.Skins.SkinManager.EnableFormSkins();
+
+            DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Darkroom");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
