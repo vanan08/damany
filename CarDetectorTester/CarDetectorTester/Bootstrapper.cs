f3f62fc9144ae27a2529440063d8b9bf4e6b0744
shenbin <shenbin@localhost>
2010-09-17 17:36:55 +0800

    ???????

---
+++ CarDetectorTester/CarDetectorTester/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+using CarDetectorTester.ViewModels;
+
+namespace CarDetectorTester
+{
+    public class Bootstrapper : Bootstrapper<ShellViewModel>
+    {
+        public Bootstrapper()
+        {
+            var i = 3;
+        }
+    }
+}
