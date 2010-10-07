c04747189312a68355c1ee775f315ec208d3262f
shenbin <shenbin@localhost>
2010-10-07 16:38:30 +0800

    ?? SquareListView ???????

--- Damany.Library/Damany.Windows.Form/ConversionHelper.cs
+++ Damany.Library/Damany.Windows.Form/ConversionHelper.cs
@@ -8,9 +8,6 @@ namespace Damany.Windows.Form
 {
     public static class ConversionHelper
     {
-        public static System.Drawing.Rectangle ToRectangle(this System.Drawing.RectangleF rectangleF)
-        {
-            return new Rectangle((int) rectangleF.X, (int) rectangleF.Y, (int) rectangleF.Width, (int) rectangleF.Height);
-        }
+        
     }
 }
