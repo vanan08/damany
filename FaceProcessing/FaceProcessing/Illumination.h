f2af4b496abc115695f2fd5449a9e51cd1a3d75f
shenbin <shenbin@localhost>
2010-11-04 15:39:53 +0800

    ????????

---
+++ FaceProcessing/FaceProcessing/Illumination.h
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "cv.h"
+
+#ifdef DLL_EXPORTS
+#define DLL_API _declspec(dllexport)
+#else
+#define DLL_API _declspec(dllimport)
+#endif
+
+namespace Damany {
+
+	namespace Imaging {
+
+		namespace FaceCompare {
+class DLL_API IlluminationNorm
+{
+public:
+	IlluminationNorm(IplImage *refImg);//≤Œøºπ‚’’
+	~IlluminationNorm();
+	void Norm(IplImage *colorIn, IplImage *colorOut);
+
+private:
+	int fSearchingMaxMin(const CvMat * mat, double * min, double * max);
+	IplImage *refLight;
+};			
+
+		}
+	}
+}
