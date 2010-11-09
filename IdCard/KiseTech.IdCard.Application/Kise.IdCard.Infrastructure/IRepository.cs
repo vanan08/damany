99d87a1211027de80b080a3a633f30bba3604f32
shenbinsc <shenbinsc@localhost>
2010-11-09 23:41:45 +0800

    add idcard solution

---
+++ IdCard/KiseTech.IdCard.Application/Kise.IdCard.Infrastructure/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kise.IdCard.Infrastructure
+{
+    public interface IRepository<T>
+    {
+        T FindBy(object id);
+        IEnumerable<T> FindBy(DateTime from, DateTime to);
+        IEnumerable<T> GetAll();
+
+        void Add(T newItem);
+        void Delete(T item);
+        void Update(T item);
+    }
+}
---
+++ IdCard/KiseTech.IdCard.Application/Kise.IdCard.Model/IdQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+
+namespace Kise.IdCard.Model
+{
+    public class IdQuery : XPObject
+    {
+        public IdcardInfo IdcardInfo
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public IdQueryResponse IdQueryResponse
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
