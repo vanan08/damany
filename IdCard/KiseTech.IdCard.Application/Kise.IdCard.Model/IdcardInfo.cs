f27058fbdec4d775d09d6061714f905466c448ca
shenbinsc <shenbinsc@localhost>
2010-11-09 23:41:45 +0800

    add idcard solution

---
+++ IdCard/KiseTech.IdCard.Application/Kise.IdCard.Model/IdcardInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+
+namespace Kise.IdCard.Model
+{
+    public class IdcardInfo : XPObject
+    {
+    }
+}
