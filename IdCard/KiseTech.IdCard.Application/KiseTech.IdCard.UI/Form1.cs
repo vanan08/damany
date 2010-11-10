5c8f198e337c96f54969a8ced450a69efc3865c6
shenbin <shenbin@localhost>
2010-11-10 18:56:11 +0800

    ui test

--- IdCard/KiseTech.IdCard.Application/KiseTech.IdCard.UI/Form1.cs
+++ IdCard/KiseTech.IdCard.Application/KiseTech.IdCard.UI/Form1.cs
@@ -6,14 +6,28 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Kise.IdCard.Infrastructure.Sms;
 
 namespace KiseTech.IdCard.UI
 {
     public partial class Form1 : Form
     {
+        ISmsService sms = new SmsService("com3", 9600);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            response.Text = "";
+            button1.Enabled = false;
+
+            var res = await sms.QueryAsync(destinationNo.Text, msgToSend.Text);
+            response.Text = res;
+
+            button1.Enabled = true;
         }
     }
 }
