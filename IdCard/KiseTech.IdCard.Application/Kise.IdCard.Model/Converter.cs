e9932967c411b9357848691781eb9192a8520aad
shenbinsc <shenbinsc@localhost>
2010-11-11 21:09:43 +0800

    add missing file

---
+++ IdCard/KiseTech.IdCard.Application/Kise.IdCard.Model/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kise.IdCard.Model
+{
+    public static class Converter
+    {
+        public static IdCardInfo ToModelIdCardInfo(this Infrastructure.CardReader.IdInfo info)
+        {
+            var v = new Model.IdCardInfo()
+                        {
+                            Address = info.Address,
+                            //BornDate = DateTime.Parse(info.BornDate),
+                            GrantDept = info.GrantDept,
+                            IdCardNo = info.IdCardNo,
+                            Name = info.Name,
+                            //ValidateFrom = DateTime.Parse(info.ValidateFrom),
+                            //ValidateUntil = DateTime.Parse(info.ValidateUntil),
+                            PhotoFilePath = info.PhotoFilePath
+                        };
+
+            return v;
+        }
+    }
+}
