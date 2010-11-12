fe9712ee4a4e596de8d966d64555eee389fbfb27
shenbinsc <shenbinsc@localhost>
2010-11-13 01:15:30 +0800

    ??????

--- IdCard/KiseTech.IdCard.Application/Kise.IdCard.Infrastructure.Test/QueryServiceTest.cs
+++ IdCard/KiseTech.IdCard.Application/Kise.IdCard.Infrastructure.Test/QueryServiceTest.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Gallio.Framework;
 using Kise.IdCard.Infrastructure.Sms;
 using MbUnit.Framework;
 using MbUnit.Framework.ContractVerifiers;
 using MbUnit.Framework.Reflection;
+using Moq;
 
 namespace Kise.IdCard.Infrastructure.Test
 {
@@ -19,8 +21,9 @@ namespace Kise.IdCard.Infrastructure.Test
             //
             // TODO: Add test logic here
             //
-
-            QueryService qs = GetQs();
+            var t = new FakeTransport();
+            t.Return(q => "");
+            QueryService qs = GetQs(t);
 
             var message = "123456";
 
@@ -40,6 +43,33 @@ namespace Kise.IdCard.Infrastructure.Test
             Assert.AreEqual(0, sn);
         }
 
+        [Test]
+        public void QueryTest()
+        {
+            var message = "1*123456";
+            var reply = "0*1*987654";
+
+            var moq = new Mock<ITransport>();
+            moq.Setup(t => t.QueryAsync("", message)).Returns(TaskEx.Run(() => reply));
+
+
+            var fakeT = new FakeTransport();
+            fakeT.Return(q => "0*abc");
+            var qs = GetQs(fakeT);
+            var result = qs.QueryAsync("", message).Result;
+            Assert.AreEqual("abc", result);
+
+            fakeT.Return(q => "0*abc");
+            result = qs.QueryAsync("", message).Result;
+            Assert.AreEqual(string.Empty, result);
+
+            fakeT.Return(q => "2*def");
+            result = qs.QueryAsync("", message).Result;
+            Assert.AreEqual("def", result);
+
+        }
+
+
         private void CallMethodGetNextSequenceNumber(QueryService qs)
         {
             Reflector.InvokeMethod(qs, "GetNextSequenceNumber");
@@ -50,10 +80,10 @@ namespace Kise.IdCard.Infrastructure.Test
             return (int)Reflector.GetField(qs, "_nextSequenceNumber");
         }
 
-        private QueryService GetQs()
+        private QueryService GetQs(ITransport t)
         {
-            var t = new Sms.FakeTransport();
             return new QueryService(t);
         }
+
     }
 }
---
+++ IdCard/KiseTech.IdCard.Application/Kise.IdCard.Infrastructure/Sms/FakeTransport.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kise.IdCard.Infrastructure.Sms
+{
+    public class FakeTransport : ITransport
+    {
+        private  Func<string, string> _replyMethod;
+
+        public void Start()
+        {
+
+        }
+
+        public async Task<string> QueryAsync(string destination, string message)
+        {
+            var rndDelay = new Random(DateTime.Now.Millisecond);
+            await TaskEx.Delay(rndDelay.Next(2000, 5000));
+
+            var reply = _replyMethod(message);
+            return reply;
+
+        }
+
+        public void Return(Func<string, string> f)
+        {
+            _replyMethod = f;
+        }
+
+        public char[] GetSplitToken()
+        {
+            return new[] { '*' };
+        }
+    }
+}
