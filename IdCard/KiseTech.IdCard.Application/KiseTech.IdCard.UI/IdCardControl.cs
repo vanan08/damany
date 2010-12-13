958bb3b7239b060f2b6e9a9870a6b350c7c6b1e7
shenbin <shenbin@localhost>
2010-12-13 18:07:09 +0800

    ??????????????

--- IdCard/KiseTech.IdCard.Application/KiseTech.IdCard.UI/IdCardControl.cs
+++ IdCard/KiseTech.IdCard.Application/KiseTech.IdCard.UI/IdCardControl.cs
@@ -22,22 +22,22 @@ namespace Kise.IdCard.UI
                 {
                     _idCardInfo = value;
 
-                    this.name.Text = _idCardInfo.Name;
-                    this.sex.Text = Model.Helper.GetSexName(_idCardInfo.SexCode);
-                    this.minority.Text = MinorityDictionary[_idCardInfo.MinorityCode];
-                    this.birthDay.Text = string.Format(BirthDayFormat, _idCardInfo.BornDate.Year, _idCardInfo.BornDate.Month, _idCardInfo.BornDate.Day);
-                    this.address.Text = _idCardInfo.Address;
-                    this.issuedBy.Text = _idCardInfo.GrantDept;
-                    this.expiry.Text = FormatDate(_idCardInfo.ValidateFrom) + " — " + FormatDate(_idCardInfo.ValidateUntil);
-                    this.idCardNo.Text = _idCardInfo.IdCardNo;
-                    this.idStatus.Text = _idCardInfo.IdStatusName;
+                   // this.name.Text = _idCardInfo.Name;
+                   // this.sex.Text = Model.Helper.GetSexName(_idCardInfo.SexCode);
+                   // this.minority.Text = MinorityDictionary[_idCardInfo.MinorityCode];
+                   // this.birthDay.Text = string.Format(BirthDayFormat, _idCardInfo.BornDate.Year, _idCardInfo.BornDate.Month, _idCardInfo.BornDate.Day);
+                   // this.address.Text = _idCardInfo.Address;
+                   // //this.issuedBy.Text = _idCardInfo.GrantDept;
+                   //// this.expiry.Text = FormatDate(_idCardInfo.ValidateFrom) + " — " + FormatDate(_idCardInfo.ValidateUntil);
+                   // this.idCardNo.Text = _idCardInfo.IdCardNo;
+                   // this.idStatus.Text = _idCardInfo.IdStatusName;
 
-                    SetColor();
+                   // SetColor();
 
-                    this.image.Image = Image.FromStream(new System.IO.MemoryStream(_idCardInfo.PhotoData));
+                   // this.image.Image = Image.FromStream(new System.IO.MemoryStream(_idCardInfo.PhotoData));
 
-                    var inpc = (INotifyPropertyChanged)_idCardInfo;
-                    inpc.PropertyChanged += new PropertyChangedEventHandler(inpc_PropertyChanged);
+                   // var inpc = (INotifyPropertyChanged)_idCardInfo;
+                   // inpc.PropertyChanged += new PropertyChangedEventHandler(inpc_PropertyChanged);
 
                 }
 
@@ -46,15 +46,15 @@ namespace Kise.IdCard.UI
 
         void inpc_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            idStatus.Text = _idCardInfo.IdStatusName;
+            //idStatus.Text = _idCardInfo.IdStatusName;
 
-            SetColor();
+            //SetColor();
         }
 
         private void SetColor()
         {
-            var isNormal = _idCardInfo.IdStatus == IdStatus.UnKnown || _idCardInfo.IdStatus == IdStatus.Normal;
-            this.idStatus.BackColor = isNormal ? _originalBkColor : Color.Red;
+            //var isNormal = _idCardInfo.IdStatus == IdStatus.UnKnown || _idCardInfo.IdStatus == IdStatus.Normal;
+            //this.idStatus.BackColor = isNormal ? _originalBkColor : Color.Red;
         }
 
         public string BirthDayFormat { get; set; }
@@ -66,7 +66,7 @@ namespace Kise.IdCard.UI
             InitializeComponent();
 
             BirthDayFormat = "{0} 年 {1} 月 {2} 日";
-            _originalBkColor = idStatus.BackColor;
+            //_originalBkColor = idStatus.BackColor;
         }
 
         public IdCardControl(IDictionary<int, string> minorityDictionary)
