9769b469547f5410be067548da5dddfb58a4d55b
shenbinsc <shenbinsc@localhost>
2010-12-28 10:01:07 +0800

    add wcf service

---
+++ IdCard/KiseTech.IdCard.Application/IdQueryService/IdQueryProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace IdQueryService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "IdQueryProvider" in both code and config file together.
+    public class IdQueryProvider : IIdQueryProvider
+    {
+        public string QueryIdCard(string queryType, string queryString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
