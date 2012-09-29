f87ede97e072455fef654fe0f86019fdf597cc09
shenbin <shenbin@localhost>
2012-09-29 12:48:28 +0800

    ??build???????advanced installer 9.5

---
+++ IdCard/KiseTech.IdCard.Application/build.bat
@@ -0,0 +1,3 @@
+DevEnv IdCard.sln /release /rebuild
+"%ProgramFiles(x86)%\Caphyon\Advanced Installer 9.5\bin\x86\AdvancedInstaller.com" /rebuild .\Setup\setup.aip
+"%ProgramFiles(x86)%\Caphyon\Advanced Installer 9.5\bin\x86\AdvancedInstaller.com" /rebuild .\Setup\server.aip
