ffbffe3bfca9631ed2d7b0820a66253a568da8ec
shenbin <shenbin@localhost>
2012-10-22 12:52:51 +0800

    ??build????

--- IdCard/KiseTech.IdCard.Application/build.bat
+++ IdCard/KiseTech.IdCard.Application/build.bat
@@ -1,3 +1,3 @@
-DevEnv IdCard.sln /release /rebuild
+DevEnv IdCard.sln /rebuild release 
 "%ProgramFiles(x86)%\Caphyon\Advanced Installer 9.5\bin\x86\AdvancedInstaller.com" /rebuild .\Setup\setup.aip
 "%ProgramFiles(x86)%\Caphyon\Advanced Installer 9.5\bin\x86\AdvancedInstaller.com" /rebuild .\Setup\server.aip
